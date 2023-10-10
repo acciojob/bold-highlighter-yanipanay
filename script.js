919fd012b57950f399cafef64f5f91bb9cd7b0d2
yani pranay <33549205+yanipanay@users.noreply.github.com>
2023-10-11 00:38:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
+
 function highlight() {
     //Write your code here
+	const boldList = document.querySelectorAll("strong");
 
-
+	for (let elem of boldList) {
+		elem.style.color ="green";
+	}
 }
 
 
 function return_normal() {
-    //Write your code here
+	const boldList = document.querySelectorAll("strong");
 
-    
+    //Write your code here
+for (let elem of boldList) {
+		elem.style.color ="black";
+	}
 }
